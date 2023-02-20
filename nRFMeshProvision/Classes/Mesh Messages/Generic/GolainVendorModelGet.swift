fb7cc55771120ace1bc6285bab01747829723603
Atharv Bhadange <bhadange.atharv@gmail.com>
2023-02-20 16:51:45 +0530

    Golain Vendor Model Get

--- nRFMeshProvision/Classes/Mesh Messages/Generic/GolainVendorModelGet.swift
+++ nRFMeshProvision/Classes/Mesh Messages/Generic/GolainVendorModelGet.swift
@@ -1,21 +1,31 @@
+//
+//  GolainVendorModelGetArguments.swift
+//  nordic_nrf_mesh
+//
+//  Created by Atharv Bhadange on 16/02/23.
+//
+
 import Foundation
 
 public struct GolainVendorModelGet: VendorMessage {
-    public let opCode: UInt32 = 0x0001
-    public static let responseType: StaticMeshMessage.Type = GolainVendorModelStatus.self as! any StaticMeshMessage.Type
+    public var opCode: UInt32 = 0x1111
 
-    public var parameters: Data? {
-        return nil
-    }
+    public var companyIdentifier: UInt16
 
-    public init() {
-        // Empty
+    public var parameters: Data?
+
+    public var isSegmented: Bool = false
+    public var security: MeshMessageSecurity = .low
+
+    public init(opCode: UInt8, companyIdentifier: UInt16, parameters: Data?) {
+        self.opCode = (UInt32(0xC0 | opCode) << 16) | UInt32(companyIdentifier.bigEndian)
+        self.parameters = parameters
+        self.companyIdentifier = companyIdentifier
     }
 
     public init?(parameters: Data) {
-        guard parameters.isEmpty else {
-            return nil
-        }
+        // This init will never be used, as it's used for incoming messages.
+        return nil
     }
 
 }
